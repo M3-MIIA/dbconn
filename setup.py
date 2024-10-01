67e00093926e4db37bef9c4c7d3e82ab3c8fc932
AriOliv <ariguilherme.aguiar@hotmail.com>
2024-10-01 20:06:01 -0300

    commit 3

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dbconn',
-    version='2.0',
-    packages=find_packages(),
+    name="dbconn",  # Nome do pacote
+    version="0.1.0",  # Versão inicial
+    description="Um pacote para gerenciar conexões de banco de dados",
+    author="M3-MIIA",
+    author_email="ari.oliveira@protonmail.com",
+    url="https://github.com/M3-MIIA/dbconn",  # URL do projeto
+    packages=find_packages(),  # Automaticamente encontra os pacotes Python
     install_requires=[
+        # Dependências do seu pacote (exemplo)
         'psycopg2-binary',
         'SQLAlchemy',
         'boto3',
         'botocore'
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Versão mínima do Python
 )
