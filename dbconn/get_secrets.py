c9532d20c0e2eeb4bb7cb20bc737c0e0a484f401
AriOliv <ariguilherme.aguiar@hotmail.com>
2025-04-04 23:16:55 -0300

    supporting proxy credentials

--- dbconn/get_secrets.py
+++ dbconn/get_secrets.py
@@ -12,7 +12,7 @@ def _parse_secret(secret_obj):
 
 def _get_secret():
 
-    secret_name = f"{service}/postgres"
+    secret_name = f"{service}/proxy/postgres"
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
