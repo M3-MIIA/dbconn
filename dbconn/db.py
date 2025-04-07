caed74e0f9e9266e89d15efd1d842d5b9c8744df
AriOliv <ariguilherme.aguiar@hotmail.com>
2025-04-07 18:48:31 -0300

    adding DB import

--- dbconn/db.py
+++ dbconn/db.py
@@ -2,9 +2,12 @@ from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from .get_secrets import get_secret
 
+DB_URL = get_secret()
+DB: AsyncEngine = create_async_engine(DB_URL,
+    connect_args={'options': '-csearch_path={}'.format('public')})
+
 def connect_to_db(tenant_id='public'):
-    DB_URL = get_secret()
-    dbschema=tenant_id # Searches left-to-right
+    dbschema=tenant_id
     DB: AsyncEngine = create_async_engine(DB_URL,
         connect_args={'options': '-csearch_path={}'.format(dbschema)})
 
