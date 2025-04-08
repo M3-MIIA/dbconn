02c54b494d1c76ba5eb21d4e02adcdb1430e0e5c
AriOliv <ariguilherme.aguiar@hotmail.com>
2025-04-08 15:43:56 -0300

    cleaning

--- dbconn/__init__.py
+++ dbconn/__init__.py
@@ -1,2 +1,2 @@
 from sqlalchemy import text
-from .db import connect_to_db, DB
+from .db import connect_to_db
--- dbconn/db.py
+++ dbconn/db.py
@@ -4,10 +4,8 @@ from .get_secrets import get_secret
 
 def connect_to_db(tenant_id='public'):
     DB_URL = get_secret()
-    dbschema=tenant_id # Searches left-to-right
+    dbschema=tenant_id
     DB: AsyncEngine = create_async_engine(DB_URL,
         connect_args={'options': '-csearch_path={}'.format(dbschema)})
 
     return DB
-
-DB = connect_to_db()
